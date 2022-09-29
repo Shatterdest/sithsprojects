1736439b5cc49d8c03601509f42dd4010eec3f4e
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2022-09-29 10:46:57 -0400

    Update numberguess.py

--- 9thgrade/numberguess/numberguess.py
+++ 9thgrade/numberguess/numberguess.py
@@ -3,38 +3,42 @@ import time
 import sys
 
 try:
+    finished = False
+    while finished == False:
+        numbers = ['1','2','3','4','5','6','7','8','9','10']
+        correctnum = int((random.choice(numbers)))
+        guess = int(0)
+        count = int(1)
+        incorrectguesses = []
+        new_line = '\n'
 
+        correct = False 
+        while correct == False:
+            guess = int(input('Input a number guess here between 1 and 10: '))
+            print('Processing.... Please wait.')
+            time.sleep(0.7)
 
-    numbers = ['1','2','3','4','5','6','7','8','9','10']
-    correctnum = int((random.choice(numbers)))
-    guess = int(0)
-    count = int(1)
-    incorrectguesses = []
-    new_line = '\n'
-
-    correct = False 
-    while correct == False:
-        guess = int(input('Input a number guess here between 1 and 10: '))
-        print('Processing.... Please wait.')
-        time.sleep(0.7)
-
-        if guess == correctnum: 
-            print(f'You have guessed the correct number! Good Job!{new_line}Your incorrect guesses will now be printed.')
-            time.sleep(2)
-            for index in incorrectguesses:
-                print(f'Incorrect guess #{count}: {index}')
-                time.sleep(1)
-                count = count + 1
-            print('Thank you for using the Number Guess program!')
-            correct = True
+            if guess == correctnum: 
+                print(f'You have guessed the correct number! Good Job!{new_line}Your incorrect guesses will now be printed.')
+                time.sleep(2)
+                for index in incorrectguesses:
+                    print(f'Incorrect guess #{count}: {index}')
+                    time.sleep(1)
+                    count = count + 1
+                correct = True
+                if_finished = input('Do you want to go again? (yes/no) ')
+                if if_finished.lower() == 'no':
+                    finished = True
+                    print('Thank you for using the Number Guess program!')
             
-        elif guess < correctnum:
-            print(f'Your guess "{guess}" is below the correct number. Try again!')
-            incorrectguesses.append(guess)
+            elif guess < correctnum:
+                print(f'Your guess "{guess}" is below the correct number. Try again!')
+                incorrectguesses.append(guess)
 
-        elif guess > correctnum:
-            print(f'Your guess "{guess}" is above the correct number. Try again!')
-            incorrectguesses.append(guess)
+            elif guess > correctnum:
+                print(f'Your guess "{guess}" is above the correct number. Try again!')
+                incorrectguesses.append(guess)
+    
 
 
 
