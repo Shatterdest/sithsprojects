0419992fa1ccffd5fabbfc4611d005e9ee057fa5
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2022-11-19 16:34:29 -0500

    lmfao

--- 9thgrade/personalprojects/pi/pi.py
+++ 9thgrade/personalprojects/pi/pi.py
@@ -1,12 +1,17 @@
-try:
-     # import version included with old SymPy
-     from sympy.mpmath import mp
-except ImportError:
-     # import newer version
-     from mpmath import mp
+import time
+from mpmath import mp
 
-mp.dps = 1000  # set number of digits
+
+input = input('How many digits of pi do you want? ')
+mp.dps = int(input)
+st = time.time()# set number of digits
 balls = mp.pi
 
-with open('C:/Users/Shatterdest/Desktop/pi', 'w') as file:
-    file.write(balls)# print pi to a thousand places
+with open('C:/Users/Shatterdest/Documents/GitHub/sithsprojects/9thgrade/personalprojects/pi/pi.txt', 'w') as file:
+    file.write(str(balls))# print pi to a thousand places
+
+et = time.time()
+
+tt = et-st
+
+print(f'Time took: {tt}')
